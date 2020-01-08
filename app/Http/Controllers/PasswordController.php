0853b869794bbb292a0f2c115cdff90983dd9bc5
Alejandro Marañes <alejandromaranes@gmail.com>
2020-01-08 14:33:56 +0100

    no message

---
+++ app/Http/Controllers/PasswordController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Password;
+use Illuminate\Http\Request;
+
+class PasswordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Password  $password
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Password $password)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Password  $password
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Password $password)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Password  $password
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Password $password)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Password  $password
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Password $password)
+    {
+        //
+    }
+}
---
+++ database/factories/LoginFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Login::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
