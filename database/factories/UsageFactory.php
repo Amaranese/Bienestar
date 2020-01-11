87d1152f862365cd0d4ee113eec7102a0b95362b
Alejandro Marañes <alejandromaranes@gmail.com>
2020-01-11 17:23:17 +0100

    no message

---
+++ database/factories/UsageFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Usage::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_01_11_115345_create_usages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('date');
+            $table->string('app');
+            $table->string('event');
+            $table->double('latitude');
+            $table->double('longitude');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usages');
+    }
+}
