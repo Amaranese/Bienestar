49776d627069f5f53229dcb51c07399d6b344924
Alejandro Marañes <alejandromaranes@gmail.com>
2020-01-15 13:52:59 +0100

    no message

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -88,46 +88,6 @@ class UserController extends Controller
     {
         //
     }
-    public function post_recover(Request $request)
-    {
-        if (!isset($_POST['email']))
-        {
-            return response()->json([
-                'MESSAGE' => 'Please enter your email address'], 403
-            );
-        }
-        $email = $_POST['email'];
-        if (self::recoverPassword($email)) {
-            $userRecover = User::where('email', $email)->first();
-            $id = $userRecover->id;
-            //$pwdDB = User::where('email', $userRecover->email)->first()->password;
-            $dataEmail = array(
-                'newPwd' => $this->randomString(),
-            );
-            if ($_POST["email"] == $userRecover->email) {
-                $userRecover->password = encrypt($dataEmail["newPwd"]);
-                $userRecover->save();
-                Mail::send('emails.welcome', $dataEmail, function($message){
-                    $emailRecipient = $_POST['email'];
-                    $message->from('bienestardigital2019@gmail.com', 'Password recovery');
-                    $message->to($emailRecipient)->subject('Recover password');
-                });
-                return response()->json([
-                    "MESSAGE" => "The new password has been sent to your email address " . $email], 200
-                );
-            }
-        }
-        else
-        {
-            return response()->json([
-                'MESSAGE' => 'The specified email does not exist'], 403
-            );
-        }
-    }
-    private function randomString($length = 30)
-    {
-        return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length)
-    }
     /**
      * Show the form for editing the specified resource.
      *
