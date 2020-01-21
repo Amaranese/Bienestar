3c121c2e658cf4a5e3699fea027526e1f7c9c20a
Alejandro Marañes <alejandromaranes@gmail.com>
2020-01-21 13:41:51 +0100

    no message

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang= "es-ES">
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h2>Contraseña</h2>
+	<div>
+		Youre new password is {!!$newPwd!!}
+	</div>
+</body>
+</html>
+
